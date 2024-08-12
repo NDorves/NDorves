7f4914952e367d2e2bc84ae02f6eb1def268fad9
NDorves <dorves@gmx.net>
2024-08-12 12:41:04 +0200

    Update HAFG_37.py

--- HAFG_37.py
+++ HAFG_37.py
@@ -8,7 +8,7 @@
 # dbconfig = {
 #     'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
 #     'user': 'ich1',
-#     'password': 'password',
+#     'password': 'xxxxxxxx',
 #     'database': 'ich_edit'
 # }
 # user = input('Введите название таблицы: ')
@@ -33,7 +33,7 @@ import mysql.connector
 dbconfig = {
     'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
     'user': 'ich1',
-    'password': 'password',
+    'password': 'xxxxxxxx',
     'database': 'ich_edit'
 }
 connection = mysql.connector.connect(**dbconfig)
