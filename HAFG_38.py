6cfc6160f2307915da9ef4b42ff9554555339613
NDorves <dorves@gmx.net>
2024-08-12 12:39:49 +0200

    Update HAFG_38.py

--- HAFG_38.py
+++ HAFG_38.py
@@ -19,7 +19,7 @@ import mysql.connector
 # dbconfig = {
 #     'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
 #     'user': 'ich1',
-#     'password': 'password',
+#     'password': 'xxxxxxx',
 #     'database': 'ich_edit'
 # }
 #
