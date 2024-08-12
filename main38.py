bebd098c205dfecb6d5dab249e41929d4659f5e2
NDorves <dorves@gmx.net>
2024-08-12 12:39:15 +0200

    Update main38.py

--- main38.py
+++ main38.py
@@ -3,7 +3,7 @@ import mysql.connector
 dbconfig = {
     'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
     'user': 'ich1',
-    'password': 'password',
+    'password': 'xxxxxx',
     'database': 'ich_edit'
 }
 
